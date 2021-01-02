b41a688386906d3d4938cbcf25f30358f7e53da2
naiko <n.buffo89@gmail.com>
2021-01-02 10:05:53 +0100

    Define Citations

--- src/app/mock/words.ts
+++ src/app/mock/words.ts
@@ -1,23 +1,24 @@
 
-export const LINKS = [
-  {id: 1, testo: '“Il mondo è un posto pericoloso, non a causa di quelli che compiono azioni malvagie ma per quelli che osservano senza fare nulla.”', firma: 'Albert Einstein'},
+export const WORDS = [
+  {id: 1, testo: '“La pace è un sogno, può diventarerealtà… Ma per costruirla bisogna essere capaci di sognare.”', firma: 'Nelson Mandela'},
   {id: 2, testo: '“Mai memorizzare quello che puoi comodamente trovare in un libro.”', firma: 'Albert Einstein'},
-  {id: 3, testo: '“Cerca di diventare non un uomo di successo ma piuttosto un uomo di valore.”', firma: 'Albert Einstein'},
-  {id: 4, testo: '“Il tempo è relativo, il suo unicovalore è dato da ciò che noi facciamo mentre sta passando.”', firma: 'Albert Einstein'},
+  {id: 3, testo: '“Strapazzando un uomo so, da come reagisce, che cosa pensare di lui. Vedo a quale tono è salita la sua anima: se colpite un bronzo con un guanto, non vi darà alcun suono; ma colpitelo con un martello e risuonerà.””', firma: 'Napoleone Buonaparte'},
+  {id: 4, testo: '““Chi ha raggiunto lo stadio di non meravigliarsi più di nulla dimostra semplicemente di aver perduto l\'arte del ragionare e del riflettere.”', firma: 'Max Planck'},
   {id: 5, testo: '“I computer sono incredibilmente veloci, accurati e stupidi. Gli uomini sono incredibilmente lenti, inaccurati e intelligenti. L\'insieme dei due costituisce una forza incalcolabile.”', firma: 'Albert Einstein'},
-  {id: 6, testo: '“L\'insegnamento deve essere tale da far percepire ciò che viene offerto come un dono prezioso, e non come un dovere imposto.”', firma: 'Albert Einstein'},
-  {id: 7, testo: '“La realtà è una semplice illusione, sebbene molto persistente.”', firma: 'Albert Einstein'},
-  {id: 8, testo: '“I grandi spiriti hanno sempre trovato la violenta opposizione dei mediocri, i quali non sanno capire l\'uomo che non accetta i pregiudizi ereditati, ma con onestà e coraggio usa la propria intelligenza.”', firma: 'Albert Einstein'},
+  {id: 6, testo: '“L\'atteggiamento di fondo della mia vita è stata la passione. Per realizzare i miei sogni ho agito sempre spinto solo dalla passione. La passione muove ogni cosa, è una forza davvero straordinaria.”', firma: 'Roberto Baggio'},
+  {id: 7, testo: '“Un buon cervello e un cuore puro sono sempre un\'accoppiata formidabile. Ma quando aggiungi a questo una voce o una penna da letterato, allora hai qualcosa di veramente speciale.”', firma: 'Nelson Mandela'},
+  {id: 8, testo: '“Lascio agli altri la convinzione di essere i migliori, per me tengo la certezza che nella vita si può sempre migliorare.”', firma: 'Marilyn Monroe'},
   {id: 9, testo: '“La teoria è quando si sa tutto ma non funziona niente. La pratica è quando tutto funziona ma non si sa il perché. In ogni caso si finisce sempre con il coniugare la teoria con la pratica: non funziona niente e non si sa il perché.”', firma: 'Albert Einstein'},
-  {id: 10, testo: '“Io so con assoluta certezza di non possedere un talento speciale; la curiosità, l\'ossessione e l\'ostinataresistenza, unita all\'autocritica, mi hanno portato alle mie idee.”', firma: 'Albert Einstein'},
-  {id: 11, testo: '“Le cose sono unite da legami invisibili: non si può cogliere un fiore senza turbare una stella.”', firma: 'Albert Einstein'},
-  {id: 12, testo: '“La sola cosa che interferisce con il mio apprendimento è la mia istruzione.”', firma: 'Albert Einstein'},
-  {id: 13, testo: '“Tutto è relativo. Prendi un ultracentenario che rompe uno specchio: sarà ben lieto di sapere che ha ancora sette anni di disgrazie.”', firma: 'Albert Einstein'},
-  {id: 14, testo: '“Tutti sanno che una cosa è impossibile da realizzare, finché arriva uno sprovveduto che non lo sa e la inventa.”', firma: 'Albert Einstein'},
-  {id: 15, testo: '“La mente è come un paracadute. Funziona solo se si apre.”', firma: 'Albert Einstein'},
-  {id: 16, testo: '“Se A è uguale a successo, allora la formula è A = X + Y + Z. Il lavoro è X; il gioco è Y; e Z è tenere la bocca chiusa.”', firma: 'Albert Einstein'},
-  {id: 17, testo: '“È un errore frequente misurare le cose in base al denaro che costano.”', firma: 'Albert Einstein'},
-  {id: 18, testo: '“La misura dell’intelligenza è data dalla capacità di cambiare quando è necessario.”', firma: 'Albert Einstein'},
-  {id: 19, testo: '“Una persona che non abbia mai commesso un errore non ha mai cercato di fare qualcosa.”', firma: 'Albert Einstein'},
-  {id: 20, testo: '“Ogni cosa che puoi immaginare la natura l\'ha già creata.”', firma: 'Albert Einstein'}
+  {id: 10, testo: '“Una nuova verità scientifica non trionfa perché i suoi oppositori si convincono e vedono la luce, quanto piuttosto perché alla fine muoiono, e al loro posto si forma una nuova generazione a cui i nuovi concetti diventano familiari.”', firma: 'Max Planck'},
+
 ];
+//  {id: 11, testo: '“Le cose sono unite da legami invisibili: non si può cogliere un fiore senza turbare una stella.”', firma: 'Albert Einstein'},
+//   {id: 12, testo: '“La sola cosa che interferisce con il mio apprendimento è la mia istruzione.”', firma: 'Albert Einstein'},
+//   {id: 13, testo: '“Tutto è relativo. Prendi un ultracentenario che rompe uno specchio: sarà ben lieto di sapere che ha ancora sette anni di disgrazie.”', firma: 'Albert Einstein'},
+//   {id: 14, testo: '“Tutti sanno che una cosa è impossibile da realizzare, finché arriva uno sprovveduto che non lo sa e la inventa.”', firma: 'Albert Einstein'},
+//   {id: 15, testo: '“La mente è come un paracadute. Funziona solo se si apre.”', firma: 'Albert Einstein'},
+//   {id: 16, testo: '“Se A è uguale a successo, allora la formula è A = X + Y + Z. Il lavoro è X; il gioco è Y; e Z è tenere la bocca chiusa.”', firma: 'Albert Einstein'},
+//   {id: 17, testo: '“È un errore frequente misurare le cose in base al denaro che costano.”', firma: 'Albert Einstein'},
+//   {id: 18, testo: '“La misura dell’intelligenza è data dalla capacità di cambiare quando è necessario.”', firma: 'Albert Einstein'},
+//   {id: 19, testo: '“Una persona che non abbia mai commesso un errore non ha mai cercato di fare qualcosa.”', firma: 'Albert Einstein'},
+//   {id: 20, testo: '“Ogni cosa che puoi immaginare la natura l\'ha già creata.”', firma: 'Albert Einstein'}
