bba2f865c2c3e5cf3747fd07751828011d173443
naiko <n.buffo89@gmail.com>
2021-01-02 17:54:58 +0100

    Add hr and fixed some layout's bug

--- src/app/mock/mock-image.ts
+++ src/app/mock/mock-image.ts
@@ -1,7 +1,7 @@
 import { BaseLinks } from '../interface/baseLinks';
 
 export const IMAGES: BaseLinks[] = [
-  { id: 1, name: 'Uno', link: '/assets/carousel/cr1.png', descr: 'Descrizione 1'},
-  { id: 3, name: 'Tre', link: '/assets/carousel/cr2.png', descr: 'Descrizione 3'},
-  { id: 4, name: 'Quattro', link: '/assets/carousel/cr3.png', descr: 'Descrizione 4'}
+  { id: 1, name: 'Linguaggi Utilizzati', link: '/assets/carousel/cr1.png', descr: 'Descrizione 1'},
+  { id: 3, name: 'Ide Utilizzati', link: '/assets/carousel/cr2.png', descr: 'Descrizione 3'},
+  { id: 4, name: 'Sistemi Operativi Utilizzati', link: '/assets/carousel/cr3.png', descr: 'Descrizione 4'}
 ];
