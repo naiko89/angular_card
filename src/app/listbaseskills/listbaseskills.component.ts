9f71cfa7023d63f1d4c844bf36e7e7c73bec82c9
naiko <n.buffo89@gmail.com>
2021-01-17 21:11:32 +0100

    animation experiment

--- src/app/listbaseskills/listbaseskills.component.ts
+++ src/app/listbaseskills/listbaseskills.component.ts
@@ -1,17 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import { ICONP } from '../mock/mock-programicon';
+import {trigger, state, style, animate, transition, stagger, keyframes, query} from '@angular/animations';
+
 
 @Component({
   selector: 'app-listbaseskills',
   templateUrl: './listbaseskills.component.html',
-  styleUrls: ['./listbaseskills.component.css']
+  styleUrls: ['./listbaseskills.component.css'],
+  animations: [
+    trigger('resizeIcon', [
+       state('closed', style({
+         height: '50px',
+         width: '50px',
+         marginTop: '5px',
+         marginRight: '120px'
+       })),
+       state('open', style({
+         height: '35px',
+         width: '35px'
+         })),
+      transition('open => closed', [
+        animate('3s', style(
+          {
+            height: '50px',
+            width: '50px',
+            marginTop: '5px',
+            marginRight: '120px'
+          }))
+        ]),
+      transition('closed => open', [
+        animate('3s', style(
+          {
+            height: '35px',
+            width: '35px'
+          }))
+      ])
+    ]),
+    trigger('resizeBoxIcon', [
+      state('closed', style({
+        marginRight: '50px'
+      })),
+      state('open', style({
+        marginRight: '0px'
+      })),
+      transition('open => closed', [
+        animate('3s', style({
+          marginRight: '50px'
+        })),
+        transition('closed => open', [
+          animate('3s', style({
+            marginRight: '0px'
+          }))
+        ])
+      ])
+    ])
+  ]
 })
 export class ListbaseskillsComponent implements OnInit {
-
   iconsp = ICONP;
-  constructor() { }
+  interval = true;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+   this.move();
+  }
+
+  move(): any{
+    let count = 0;
+    const interval = setInterval(() => {
+      if ( count === 1 ) {
+        this.interval = false;
+      }
+      else if (count === 5)
+        { count = 0;
+          this.interval = true;
+        }
+      count++;
+      }, 2000);
+  }
 }
